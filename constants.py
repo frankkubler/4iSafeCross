d45f9de820f10a64c306e546275d033db29cc6b3
4itec-mu <4itec4.0@gmail.com>
2025-07-24 08:58:35 +0200

    Update constants.py
    update zone 3

--- constants.py
+++ constants.py
@@ -24,11 +24,13 @@ ZONES_DEFAULT = [
     {"name": "zone2", "rect": (641, 0, 1280, 480), "color": (0, 255, 255)},   # Jaune
     {"name": "zone1", "rect": (0, 481, 640, 960), "color": (255, 0, 255)},    # Magenta
 ]
-
 # Définition des zones de détection par caméra (exemple pour 2 caméras)
 ZONES_BY_CAMERA = {
     0: [
-        {"name": "zone3_cam0", "rect": (0, 0, 1920, 180), "color": (255, 255, 0)},
+        # {"name": "zone3_cam0", "rect": (0, 0, 1920, 180), "color": (255, 255, 0)},
+        {"name": "zone3_cam0", "polygon": [(1105, 30), (1103, 110), (1238, 290), (1887, 1074), (1, 1068),
+                                            (1, 388), (443, 388), (441, 225), (743, 225), (743, 32)],
+                                            "color": (255, 255, 0)},
         {"name": "zone2_cam0", "polygon": [(1200, 80), (1500, 80), (1920, 800), (1800, 1080)], "color": (0, 255, 255)},# Cyan
         # {"name": "zone2_cam0", "rect": (0, 180, 1920, 740), "color": (0, 255, 255)},   # Jaune
         # {"name": "zone1_cam0", "rect": (0, 380, 1920, 1080), "color": (255, 0, 255)},  # Magenta
