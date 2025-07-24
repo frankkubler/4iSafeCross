234933028b09e22742912c79202e52f80ab61486
4itec-mu <4itec4.0@gmail.com>
2025-07-24 08:35:29 +0200

    Update constants.py
    update zones

--- constants.py
+++ constants.py
@@ -29,9 +29,10 @@ ZONES_DEFAULT = [
 ZONES_BY_CAMERA = {
     0: [
         {"name": "zone3_cam0", "rect": (0, 0, 1920, 180), "color": (255, 255, 0)},
-        {"name": "zone2_cam0", "polygon": [(1200, 80), (1600, 80), (1920, 540), (1600, 1080)], "color": (0, 255, 255)},# Cyan
+        {"name": "zone2_cam0", "polygon": [(1200, 80), (1600, 80), (1920, 800), (1800, 1080)], "color": (0, 255, 255)},# Cyan
         # {"name": "zone2_cam0", "rect": (0, 180, 1920, 740), "color": (0, 255, 255)},   # Jaune
-        {"name": "zone1_cam0", "rect": (0, 380, 1920, 1080), "color": (255, 0, 255)},  # Magenta
+        # {"name": "zone1_cam0", "rect": (0, 380, 1920, 1080), "color": (255, 0, 255)},  # Magenta
+        {"name": "zone1_cam0", "polygon": [(0, 380), (1700, 380), (1800, 1080), (0, 1080)], "color": (255, 0, 255)},  # Magenta
     ],
     1: [
         {"name": "zone3_cam1", "rect": (0, 0, 800, 600), "color": (255, 255, 0)},      # Cyan
