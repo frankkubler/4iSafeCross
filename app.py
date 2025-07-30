7bd5e027b16ab5d8768dfce4651a7b7f5ed3a1a5
frankkubler <kubler.f@gmail.com>
2025-07-30 17:09:16 +0200

    using waitress

--- app.py
+++ app.py
@@ -496,4 +496,5 @@ def cam_status(cid):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5050, threaded=True)
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=5050)
