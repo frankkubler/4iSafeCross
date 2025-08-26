451c43b7119f3addb945e90baf1dc818aef55ecd
kubikub <kubikub@gmail.com>
2025-08-26 11:00:52 +0200

    fix tracker_id bug

--- src/inference.py
+++ src/inference.py
@@ -101,7 +101,7 @@ class InferenceServerThread(threading.Thread):
                                 "confidence": float(d["confidence"]),
                                 "class_id": int(d["class_id"]),
                                 "label": d.get("label", ""),
-                                "tracker_id": int(d.get("tracker_id", -1)),
+                                "tracker_id": int(d.get("tracker_id") or -1),  # Utilise 'or -1' au lieu de la valeur par défaut
                                 "personne_type": (d.get("personne_type") if (d.get("personne_type") in ("sitting_in_vehicle", "pieton")) else ("pieton" if int(d["class_id"]) == 1 else ""))
                             }
                             for d in detections if d["class_id"] in self.class_id or DETECTION == 'extended'
