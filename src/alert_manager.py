d5654ec767513a451ad9c5d68071538c6d2a6195
frankkubler <kubler.f@gmail.com>
2026-03-13 08:04:49 +0100

    rolback to v1.0.1

--- src/alert_manager.py
+++ src/alert_manager.py
@@ -129,7 +129,8 @@ class AlerteManager:
         if detections:
             for det in detections:
                 if isinstance(det, dict) and "zones" in det:
-                    zone_names_detected.update(det["zones"])
+                    if det.get("label") == "person":
+                        zone_names_detected.update(det["zones"])
         # Activation des relais pour chaque zone détectée
         for zone_name in zone_names_detected:
             relay_nums = self._get_relay_nums_from_zone(zone_name)
